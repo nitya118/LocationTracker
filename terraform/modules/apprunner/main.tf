71df7b20b442a3580b83e41a5c22a3db9476cdfb
Nitya Shankar <NH118840@hertz.net>
2023-10-11 22:10:59 +0200

    order changed

--- terraform/modules/apprunner/main.tf
+++ terraform/modules/apprunner/main.tf
@@ -1,40 +1,5 @@
 
 
-resource "aws_apprunner_service" "apprunner_backend" {
-
-  depends_on = [time_sleep.waitrolecreate]
-
-  service_name    = var.service_name
-
-  auto_scaling_configuration_arn = aws_apprunner_auto_scaling_configuration_version.app_runner_autoscaling.arn
-
-  instance_configuration {
-    cpu     = 1024
-    memory  = 2048
-    instance_role_arn = aws_iam_role.apprunner_ecr_service_instance_role.arn
-  }
-
-  source_configuration {
-    authentication_configuration {
-      access_role_arn = aws_iam_role.apprunner_ecr_service_instance_role.arn
-    }
-
-    image_repository {
-      image_configuration {
-        port = "80"
-        runtime_environment_variables = {
-          ASPNETCORE_ENVIRONMENT = var.asp_core_environment
-          ASPNETCORE_FORWARDEDHEADERS_ENABLED	= true 
-          }
-      }
-      image_identifier      = var.ecr_image_address
-      image_repository_type = "ECR"
-    }
-    auto_deployments_enabled = true
-  }
-  tags = var.tags
-}
-
 resource "aws_apprunner_auto_scaling_configuration_version" "app_runner_autoscaling" {
   auto_scaling_configuration_name = "MinConfiguration"
   max_concurrency = 100
@@ -108,3 +73,38 @@ create_duration = "60s"
 
 
 
+
+resource "aws_apprunner_service" "apprunner_backend" {
+
+  depends_on = [time_sleep.waitrolecreate]
+
+  service_name    = var.service_name
+
+  auto_scaling_configuration_arn = aws_apprunner_auto_scaling_configuration_version.app_runner_autoscaling.arn
+
+  instance_configuration {
+    cpu     = 1024
+    memory  = 2048
+    instance_role_arn = aws_iam_role.apprunner_ecr_service_instance_role.arn
+  }
+
+  source_configuration {
+    authentication_configuration {
+      access_role_arn = aws_iam_role.apprunner_ecr_service_instance_role.arn
+    }
+
+    image_repository {
+      image_configuration {
+        port = "80"
+        runtime_environment_variables = {
+          ASPNETCORE_ENVIRONMENT = var.asp_core_environment
+          ASPNETCORE_FORWARDEDHEADERS_ENABLED	= true 
+          }
+      }
+      image_identifier      = var.ecr_image_address
+      image_repository_type = "ECR"
+    }
+    auto_deployments_enabled = true
+  }
+  tags = var.tags
+}
