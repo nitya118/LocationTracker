847cac8e949b6073d1ae4ebbdb121211b73bf705
Ash Ariyapala <ashith.ariyapala@postoffice.co.uk>
2023-10-12 12:57:10 +0000

    first iteration of the api

--- api/LocationTrackerAPI/Functions.cs
+++ api/LocationTrackerAPI/Functions.cs
@@ -1,8 +1,10 @@
-using System.Net;
-using Amazon.Lambda.Core;
-using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Annotations;
 using Amazon.Lambda.Annotations.APIGateway;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using LocationTrackerAPI.Models;
+using LocationTrackerLib.Services;
+using System.Text.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -11,48 +13,120 @@ namespace LocationTrackerAPI;
 
 public class Functions
 {
+    private readonly ILocationReportDataService _locationReportDataService;
+
+    private readonly ITimeService _timeService;
+
     /// <summary>
     /// Default constructor that Lambda will invoke.
     /// </summary>
-    public Functions()
+    ///
+    public Functions() : this(null, null)
     {
     }
 
+    public Functions(ILocationReportDataService locationReportDataService, ITimeService timeService)
+    {
+        _locationReportDataService = _locationReportDataService ?? new DdbLocationReportDataService();
+
+        _timeService = timeService ?? new TimeService();
+    }
+
+    private APIGatewayProxyResponse CreateNotFoundResponse()
+    {
+        return new APIGatewayProxyResponse()
+        {
+            StatusCode = 404,
+        };
+    }
+
+    private APIGatewayProxyResponse CreateOkResponse()
+    {
+        return new APIGatewayProxyResponse()
+        {
+            StatusCode = 200,
+        };
+    }
 
     /// <summary>
     /// A Lambda function to respond to HTTP Get methods from API Gateway
     /// </summary>
     /// <remarks>
-    /// This uses the <see href="https://github.com/aws/aws-lambda-dotnet/blob/master/Libraries/src/Amazon.Lambda.Annotations/README.md">Lambda Annotations</see> 
+    /// This uses the <see href="https://github.com/aws/aws-lambda-dotnet/blob/master/Libraries/src/Amazon.Lambda.Annotations/README.md">Lambda Annotations</see>
     /// programming model to bridge the gap between the Lambda programming model and a more idiomatic .NET model.
-    /// 
+    ///
     /// This automatically handles reading parameters from an APIGatewayProxyRequest
     /// as well as syncing the function definitions to serverless.template each time you build.
-    /// 
-    /// If you do not wish to use this model and need to manipulate the API Gateway 
+    ///
+    /// If you do not wish to use this model and need to manipulate the API Gateway
     /// objects directly, see the accompanying Readme.md for instructions.
     /// </remarks>
     /// <param name="context">Information about the invocation, function, and execution environment</param>
     /// <returns>The response as an implicit <see cref="APIGatewayProxyResponse"/></returns>
     [LambdaFunction(Policies = "AWSLambdaBasicExecutionRole", MemorySize = 256, Timeout = 30)]
-    [RestApi(LambdaHttpMethod.Get, "/")]
-    public IHttpResult Get(APIGatewayProxyRequest request, ILambdaContext context)
-    { 
-        context.Logger.LogInformation("Handling the 'Get' Request");
+    [RestApi(LambdaHttpMethod.Any, "/")]
+    public async Task<APIGatewayProxyResponse> LocationReport(APIGatewayProxyRequest request, ILambdaContext context)
+    {
+        // context.Logger.LogInformation("Handling the 'Get' Request");
 
-        string? id = null;
+        var id = request.QueryStringParameters?["id"];
 
-        request.QueryStringParameters?.TryGetValue("id", out id);
-
-        if (id.Trim() == "5")
+        if (!string.IsNullOrEmpty(id))
         {
-            return HttpResults.Ok($"Hello AWS Serverless-{id}");  
+            var ls = await _locationReportDataService.LoadRecordAsync(id);
+
+            if (ls == null)
+            {
+                return CreateNotFoundResponse();
+            }
+
+            if (ls.Status != LocationTrackerLib.Models.LocationReportStatus.SMS_SENT)
+            {
+                return CreateNotFoundResponse();
+            }
+
+            return CreateOkResponse();
         }
         else
         {
-            return HttpResults.NotFound();
-        }
+            if (string.IsNullOrEmpty(request.Body))
+            {
+                return CreateNotFoundResponse();
+            }
 
-       
+            var obj = JsonSerializer.Deserialize<LocationInfo>(request.Body);
+
+            if (obj == null)
+            {
+                return CreateNotFoundResponse();
+            }
+
+            if (string.IsNullOrEmpty(obj.Id))
+            {
+                return CreateNotFoundResponse();
+            }
+
+            var ls = await _locationReportDataService.LoadRecordAsync(obj.Id);
+
+            if (ls == null)
+            {
+                return CreateNotFoundResponse();
+            }
+
+            if (ls.Status != LocationTrackerLib.Models.LocationReportStatus.SMS_SENT)
+            {
+                return CreateNotFoundResponse();
+            }
+
+            //update the record
+            ls.Status = LocationTrackerLib.Models.LocationReportStatus.LOCATION_RECEIVED;
+            ls.Lat = obj.Lat;
+            ls.Long = obj.Lon;
+            ls.LocationupdatedUTCDatetime = _timeService.GetCurrentUTCDateTime();
+
+            await _locationReportDataService.SaveRecordAsync(ls);
+
+            return CreateOkResponse();
+        }
     }
 }
