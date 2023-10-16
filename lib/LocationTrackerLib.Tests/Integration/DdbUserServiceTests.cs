c329a305a42cc2b22d10ccd934111ee451894edc
Ash Ariyapala <ashith.ariyapala@postoffice.co.uk>
2023-10-16 13:30:43 +0000

    added user data service

---
+++ lib/LocationTrackerLib.Tests/Integration/DdbUserServiceTests.cs
@@ -0,0 +1,55 @@
+﻿using LocationTrackerLib.Models;
+using LocationTrackerLib.Services;
+using Xunit;
+
+namespace LocationTrackerLib.Tests.Integration
+{
+    public class DdbUserServiceTests
+    {
+        [Xunit.Fact]
+        public async Task Should_save_a_user()
+        {
+            var sut = new DdbUserService();
+
+            var user = new User()
+            {
+                UserName = "alice@contoso.com",
+                CanLogin = true,
+                CanSendSms = true,
+                IsSupervisor = false,
+            };
+
+            await sut.SaveUser(user);
+
+            var saved = await sut.LoadUser(user.UserName);
+
+            Assert.NotNull(saved);
+        }
+
+        [Fact]
+        public async Task Should_be_able_to_delete_a_user()
+        {
+            var sut = new DdbUserService();
+
+            var user = new User()
+            {
+                UserName = "delete@contoso.com",
+                CanLogin = true,
+                CanSendSms = true,
+                IsSupervisor = false,
+            };
+
+            await sut.SaveUser(user);
+
+            var saved = await sut.LoadUser(user.UserName);
+
+            Assert.NotNull(saved);
+
+            await sut.DeleteUser(user);
+
+            var ret =await sut.LoadUser(user.UserName);
+
+            Assert.Null(ret);
+        }
+    }
+}
---
+++ lib/LocationTrackerLib/Services/DdbUserService.cs
@@ -0,0 +1,44 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using LocationTrackerLib.Models;
+
+namespace LocationTrackerLib.Services
+{
+    public class DdbUserService : IUserDataService
+    {
+        private readonly AmazonDynamoDBClient _dbClient;
+
+        private const string TABLE_NAME = "LocationReports";
+
+        public DdbUserService()
+        {
+            _dbClient = new AmazonDynamoDBClient();
+        }
+
+        public async Task DeleteUser(User user)
+        {
+            using (var dbContext = new DynamoDBContext(_dbClient))
+            {
+                await dbContext.DeleteAsync(user);
+            }
+        }
+
+        public async Task<User> LoadUser(string username)
+        {
+            using (var dbContext = new DynamoDBContext(_dbClient))
+            {
+                var user = await dbContext.LoadAsync<User>(username);
+
+                return user;
+            }
+        }
+
+        public async Task SaveUser(User user)
+        {
+            using (var dbContext = new DynamoDBContext(_dbClient))
+            {
+                await dbContext.SaveAsync(user);
+            }
+        }
+    }
+}
