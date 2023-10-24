d3c696b6b6ad60a15a57732755ac5aef6213db3c
Nitya Shankar <nitya_shankar@outlook.com>
2023-10-24 14:45:15 +0200

    dbname changs in data block

--- back-end/terraform/env/dev/data.tf
+++ back-end/terraform/env/dev/data.tf
@@ -1,9 +1,9 @@
 data "aws_dynamodb_table" "dynamodb_loc_report_arn" {
-  name = "eu01-dev01-ddb01"
+  name = "LocationReports"
 }
 
 data "aws_dynamodb_table" "dynamodb_users_arn" {
-  name = "eu01-dev01-ddb02"
+  name = "Users"
 }
 
 data "aws_ecr_repository" "ecr_repo_arn" {
